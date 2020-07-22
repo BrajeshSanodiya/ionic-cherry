c10102dba6f9e281536574134b3211752b0b50f1
Brajesh Sanodiya <59686303+brajeshsanodiya-db@users.noreply.github.com>
2020-07-22 11:19:23 +0530

    first commit

---
+++ src/components/share/share.module.ts
@@ -0,0 +1,50 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { IonicModule } from '@ionic/angular';
+import { FormsModule } from '@angular/forms';
+//for home banner
+import { BannerComponent } from '../banner/banner.component';
+//for home footer segment
+
+// for product
+import { ProductComponent } from '../product/product.component';
+//for sliding tab
+import { SlidingTabsComponent } from '../sliding-tabs/sliding-tabs.component';
+//for featrued product scrolling
+import { ScrollingFeaturedProductsComponent } from '../scrolling-featured-products/scrolling-featured-products.component';
+//for categories
+import { CategoriesComponent } from '../categories/categories.component';
+import { PipesModule } from 'src/pipes/pipes.module';
+import { TimerComponent } from '../timer/timer.component';
+import { GoogleMapComponent } from '../google-map/google-map.component';
+
+
+
+
+@NgModule({
+  declarations: [
+    BannerComponent,
+    ProductComponent,
+    SlidingTabsComponent,
+    ScrollingFeaturedProductsComponent,
+    CategoriesComponent,
+    TimerComponent,
+    GoogleMapComponent
+  ],
+  exports: [
+    BannerComponent,
+    ProductComponent,
+    SlidingTabsComponent,
+    ScrollingFeaturedProductsComponent,
+    CategoriesComponent,
+    TimerComponent,
+    GoogleMapComponent
+  ],
+  imports: [
+    IonicModule,
+    CommonModule,
+    FormsModule,
+    PipesModule,
+  ],
+})
+export class ShareModule { }
