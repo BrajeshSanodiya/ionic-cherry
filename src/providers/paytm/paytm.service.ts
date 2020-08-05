97c838578cdd7e1764f68ced9c7bfd701b4fa90d
vikky choudhary <vchoudhary0710@gmail.com>
2020-08-05 16:22:05 +0530

    update paytm.ts

--- src/providers/paytm/paytm.service.ts
+++ src/providers/paytm/paytm.service.ts
@@ -20,42 +20,47 @@ export class PaytmService {
 
       var completeUrl = paytmUrl + "/order/process";
       var pageContetn = `<html>
-              <head>
-                <title></title>
-              </head>
-              <body>
-                <form method="post" action="${completeUrl}" name="paytm">
-                    <table border="1">
-                      <tbody>
-                          <input type="hidden" name="MID" value="${mId}">
-                          <input type="hidden" name="WEBSITE" value="WEBSTAGING">
-                          <input type="hidden" name="ORDER_ID" value="${orderId}">
-                          <input type="hidden" name="CUST_ID" value="${customerId}">
-                          <input type="hidden" name="INDUSTRY_TYPE_ID" value="Retail">
-                          <input type="hidden" name="CHANNEL_ID" value="WAP">
-                          <input type="hidden" name="TXN_AMOUNT" value="${amount}">
-                          <input type="hidden" name="CALLBACK_URL" value="${callBackUrl}">
-                          <input type="hidden" name="CHECKSUMHASH" value="${chcksum}">
-                      </tbody>
-                    </table>
-                    <script type="text/javascript">
-                      document.paytm.submit();
-                    </script>
-                </form>
-              </body>
-          </html>`
+   <head>
+      <title>Show Payment Page</title>
+   </head>
+   <body>
+      <center>
+         <h1>Please do not refresh this page...</h1>
+      </center>
+      <form method="post" action="https://securegw.paytm.in/theia/api/v1/showPaymentPage?mid=${mId}&orderId=${orderId}" name="paytm">
+         <table border="1">
+            <tbody>
+               <input type="hidden" name="mid" value="${mId}">
+               <input type="hidden" name="orderId" value="${orderId}">
+               <input type="hidden" name="txnToken" value="${chcksum}">
+            </tbody>
+         </table>
+         <script type="text/javascript"> document.paytm.submit(); </script>
+      </form>
+   </body>
+</html>`
+		  
+		  
       var pageContentUrl = "data:text/html;base64," + btoa(pageContetn);
       console.log(pageContentUrl)
 
       let options: InAppBrowserOptions = {
-        location: 'yes',//Or 'no' 
-        hidden: 'no', //Or  'yes'
-        clearcache: 'yes',
-        clearsessioncache: 'yes',
-        zoom: 'no',//Android only ,shows browser zoom controls 
-        disallowoverscroll: 'no', //iOS only 
-        toolbar: 'yes', //iOS only  
-        hideurlbar: "yes"
+        
+	location : 'yes',//Or 'no' 
+    hidden : 'no', //Or  'yes'
+    clearcache : 'yes',
+    clearsessioncache : 'yes',
+    zoom : 'yes',//Android only ,shows browser zoom controls 
+    hardwareback : 'yes',
+    mediaPlaybackRequiresUserAction : 'no',
+    shouldPauseOnSuspend : 'no', //Android only 
+    closebuttoncaption : 'Close', //iOS only
+    disallowoverscroll : 'no', //iOS only 
+    toolbar : 'yes', //iOS only 
+    enableViewportScale : 'no', //iOS only 
+    allowInlineMediaPlayback : 'no',//iOS only 
+    presentationstyle : 'pagesheet',//iOS only 
+    fullscreen : 'yes',//Windows only   
       };
 
       const bb = this.iab.create(pageContentUrl, "_blank", options)
